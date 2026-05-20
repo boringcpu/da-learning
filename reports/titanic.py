05f26ce4090ebb65d93c93a936f91b9931d493e4
boringcpu <falconx2011@gmail.com>
2026-05-20 19:16:11 +0800

    add pic

--- reports/titanic.py
+++ reports/titanic.py
@@ -8,7 +8,7 @@ plt.rcParams['font.sans-serif'] = [u'SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
 
-url=r'D:\BC53\我的文档\XTC\PYTHON\titanic'
+url=r'C:\tmp\Titanic'
 for dirname, _, filenames in os.walk(url):
     for filename in filenames:
         print(os.path.join(dirname, filename))
@@ -25,62 +25,80 @@ train_data.isnull().sum()
 # 不使用Cabin变量，age以中位数填充
 
 # 目标变量分布
-for i in train_data.columns:
-   sns.countplot(x=i,data=train_data)
+cat_cols = train_data.select_dtypes(
+    include=['object']
+).columns.tolist()
+cat_cols += ['Pclass','Survived']
+for i in cat_cols:
+   sns.countplot(x=i,hue='Survived',data=train_data) # 分类
    plt.show()
+# 男性死亡率高，女性存活率高
+# 头等舱存活率高，三等舱死亡率高
 
 # 数值列
 num_cols = train_data.select_dtypes(
   include=['int64','float64']
 ).columns
+remove_cols = ['PassengerId','Survived']
+num_cols = [
+    col for col in num_cols
+    if col not in remove_cols
+]
 for col in num_cols:
-  print("="*30)
-  print(col)
-  # 基础统计
-  # print(train_data[col].describe())
-  # 偏度，≈0——接近正泰，>0——右偏（长尾在右），>1——明显偏态，>2——严重偏态
-  skew = train_data[col].skew()
-  print("偏度:", round(skew,2))
-  # 大部分人都没有亲属，票价较低，少数人有亲属&高票价
-  # IQR异常值
-  Q1 = train_data[col].quantile(0.25)
-  Q3 = train_data[col].quantile(0.75)
-  IQR = Q3 - Q1
-  lower = Q1 - 1.5*IQR
-  upper = Q3 + 1.5*IQR
-  outlier_num = (
-      (train_data[col]<lower)|(train_data[col]>upper)
-  ).sum()
-  print("异常值数量:", outlier_num)
-
-  # 分布图
-  plt.figure(figsize=(8,4))
-  plt.hist(
-      train_data[col].dropna(),
-      bins=30
-  )
-  plt.title(
-      f'{col}\nSkew={skew:.2f}'
-  )
-  plt.show()
-  # 箱线图
-  plt.figure(figsize=(8,2))
-  plt.boxplot(
-      train_data[col].dropna(),
-      vert=False # 是否垂直
-  )
-  plt.title(col)
-  plt.show()
+    print("="*30)
+    print(col)
+    # 基础统计
+    # print(train_data[col].describe())
+    # 偏度是在看一个变量自身分布是不是对称，
+    # ≈0——接近正态，>0——右偏（长尾在右），>1——明显偏态，>2——严重偏态
+    skew = train_data[col].skew()
+    print("偏度:", round(skew,2))
+    # Fare，SibSp，Parch严重右偏
+    # 大部分人都没有亲属，票价较低，少数人有亲属&高票价
+    # IQR异常值
+    Q1 = train_data[col].quantile(0.25)
+    Q3 = train_data[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5*IQR
+    upper = Q3 + 1.5*IQR
+    outlier_num = (
+        (train_data[col]<lower)|(train_data[col]>upper)
+    ).sum()
+    print("异常值数量:", outlier_num)
+    
+    # 分布图
+    plt.figure(figsize=(8,4))
+    plt.hist( # 连续
+        train_data[col].dropna(),
+        bins=30
+    )
+    plt.title(
+        f'{col}\nSkew={skew:.2f}'
+    )
+    plt.show()
+    # 箱线图
+    plt.figure(figsize=(8,2))
+    plt.boxplot(
+        train_data[col].dropna(),
+        vert=False # 是否垂直
+    )
+    plt.title(col)
+    plt.show()
 # 类别相对平衡
 
+# Pearson 相关系数默认假设连续变量、线性关系
 corr_matrix = train_data[num_cols].corr()
-# Fare与Pclass强负相关
+mask=np.triu(
+    np.ones_like(corr_matrix,dtype=bool)
+) # mask 中非零值对应的单元格会被隐藏
+# Fare与Pclass强负相关，高等级舱位票价更高
 # 热力图
 # filtered = corr_matrix.copy() # 只保留强相关
 # filtered[abs(filtered) < 0.5] = np.nan
 sns.heatmap(
     # filtered,
     corr_matrix,
+    mask=mask,
     annot=True,        # 显示数值
     cmap='coolwarm',   # 配色
     center=0,          # 0为中心
@@ -89,12 +107,31 @@ sns.heatmap(
 plt.title("Correlation Heatmap")
 plt.show()
 
-
+plt.figure(figsize=(8,5))
 plt.scatter(train_data['Age'],train_data['Fare'],marker='.',
-           cmap = 'Reds',
-           alpha = 0.8,)
+            c=train_data['Survived'],
+            cmap = 'Reds',
+            alpha = 0.8,) # 颜色表示是否生存
+plt.xlabel("Age")
+plt.ylabel("Fare")
+plt.title(
+    "Age vs Fare by Survival"
+)
+plt.colorbar(
+    label='Survived'
+)
+plt.show()
 
-
+plt.figure(figsize=(8,5))
+sns.boxplot(
+    x='Survived',
+    y='Age',
+    data=train_data
+)
+plt.title(
+    "Age vs Survival"
+)
+plt.show()
 # 数值特征分布分析主要关注偏态、异常值和长尾现象。
 # 通常通过统计偏度、箱线图和直方图进行分析。
 # 如果存在严重长尾分布，可以考虑对数变换；
@@ -124,26 +161,65 @@ y_predict = model.predict(X)
 predictions = model.predict(X_test)
 
 pipe_clf = Pipeline([
-        ('sc',StandardScaler()),
-        ('power_trans',PowerTransformer()),
-        ('polynom_trans',PolynomialFeatures(degree=3)),
-        # ('logistic_clf', LogisticRegression(penalty='l1', fit_intercept=True, 
-        #                                     solver='liblinear'))
-        ('rf',RandomForestClassifier(n_estimators=100, max_depth=5, 
-                                     random_state=1))
-        ])
-# 查看模型表现
-import warnings
+      ('sc',StandardScaler()),
+      ('power_trans',PowerTransformer()),
+      ('polynom_trans',PolynomialFeatures(degree=3)),
+      ('logistic_clf', LogisticRegression(penalty='l1', fit_intercept=True,
+                                           solver='liblinear'))
+      # ('rf',RandomForestClassifier(n_estimators=100, max_depth=5,
+      #                              random_state=1))
+      ])
+
+# 混淆矩阵，准确率，精准率，召回率，roc，auc
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
-warnings.filterwarnings('ignore')
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+import matplotlib.pyplot as plt
+
 pipe_clf.fit(X,y)
 y_predict = pipe_clf.predict(X)
+print(f'confusion_matrix：{confusion_matrix(y,y_predict)}')
 print(f'accuracy score is: {accuracy_score(y,y_predict)}')
 print(f'precision score is: {precision_score(y,y_predict)}')
 print(f'recall score is: {recall_score(y,y_predict)}')
 print(f'auc: {roc_auc_score(y,y_predict)}')
 
-output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv(r'D:\BC53\我的文档\XTC\tmp\my_submission.csv', index=False)
-print("Your submission was successfully saved!")
+# logistic_clf = pipe_clf.named_steps['logistic_clf']
+# ROC
+y_hat = pipe_clf.predict_proba(X)[:, 1] #各类别的概率
+fpr, tpr, thresholds = roc_curve(y,y_hat)
+roc_auc = auc(fpr, tpr)
+plt.figure(figsize=(6,5))
+plt.plot(fpr, tpr, color='darkorange', lw=2,
+        label=f'ROC curve (AUC = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC)')
+plt.legend(loc="lower right")
+plt.grid(alpha=0.3)
 
+# 转 DataFrame
+feat_imp = pd.DataFrame({
+    "feature": pipe_clf[:-1].get_feature_names_out(),
+    "importance": importance
+})
+
+# 排序
+feat_imp = feat_imp.sort_values(
+    by="importance",
+    ascending=False
+)
+
+# 画图
+plt.barh(
+    feat_imp["feature"],
+    feat_imp["importance"]
+)
+
+plt.show()
+
+#output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+#output.to_csv(r'D:\BC53\我的文档\XTC\tmp\my_submission.csv', index=False)
+#print("Your submission was successfully saved!")
